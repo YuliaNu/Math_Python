5b52eee16da9ca1b278604d54f11eb1221e017ca
Yulia <juliamariash@yahoo.com>
2017-11-29 21:06:28 +0200

    chages

--- Math_Python/ReadDataFromFile.py
+++ Math_Python/ReadDataFromFile.py
@@ -6,3 +6,28 @@ print(type(file_obj))
 
 #Print all data from Robert_Frost.txt
 print(file_obj.read())
+
+#Close file
+file_obj.close()
+
+#Read by line
+file_obj=open('Robert_Frost.txt','r')
+for line in file_obj:
+    print(line.strip())
+
+#Create data list
+file_object=open('Robert_Frost.txt','r')
+data_list=list(file_obj)
+
+#Close file
+file_obj.close()
+
+
+for line in data_list:
+    print(line)
+
+#Close file
+file_obj.close()
+
+
+
